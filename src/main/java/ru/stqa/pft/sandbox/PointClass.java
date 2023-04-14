4732fcc773825d6192c781b40ee45aa4bc6142a7
Alex_Donskov <mrad@list.ru>
2023-04-14 13:15:14 +0300

    Подправил формулу в классе PointClass

--- src/main/java/ru/stqa/pft/sandbox/PointClass.java
+++ src/main/java/ru/stqa/pft/sandbox/PointClass.java
@@ -8,6 +8,6 @@ public class PointClass {
         Point p1 = new Point(2, 3);
         Point p2 = new Point(3, 5);
 
-        System.out.println("Расстояние между точками" + "(" + p1.x + "," + p1.y + ")" + "и" + "(" + p2.x + "," + p2.y + ")" + "=" + p1.distance(p2));
+        System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + p1.distance(p2));
     }
 }
